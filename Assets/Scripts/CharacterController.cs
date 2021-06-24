aa0c99e0e0ef7b0016d2ea8be3ff10fa7b51ebb5
azHq <35559594+azHq@users.noreply.github.com>
2021-06-24 14:59:12 +0600

    shop dresses,shirt,shoes,cap added

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,25 +7,47 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private Camera mainCamera;
     [SerializeField]
+    private SpriteRenderer cap;
+    [SerializeField]
+    private SpriteRenderer shirt;
+    [SerializeField]
+    private SpriteRenderer pant;
+    [SerializeField]
+    private SpriteRenderer left_shoe;
+    [SerializeField]
+    private SpriteRenderer right_shoe;
+    [SerializeField]
+    private Transform _trailPosition;
+    [SerializeField]
+    private Animation _animation;
+    [SerializeField]
     private float _moveSpeed=5f;
     private Rigidbody2D _rigidbody2d;
     private Animator _animator;
     private Vector2 _moveDirection;
     private Transform _transform;
+    private bool _insideShop;
+    private GameManager gameManager;
+    ProductType productType = ProductType.CAP;
     private void Awake()
     {
         _rigidbody2d = GetComponent<Rigidbody2D>();
         _transform= GetComponent<Transform>();
         _animator = GetComponent<Animator>();
     }
-    
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
     void Update()
     {
         float moveX = 0f;
         float moveY = 0f;
+        _animator.enabled = true;
         if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow))
         {
             moveY = 1f;
+            ChangePlayerLocalScale(1);
             _animator.SetBool("forward", false);
             _animator.SetBool("walk", false);
             _animator.SetBool("back", true);
@@ -57,8 +79,6 @@ public class CharacterController : MonoBehaviour
         else
         {
             _animator.SetBool("walk", false);
-            _animator.SetBool("forward", false);
-            _animator.SetBool("back", false);
         }
         _moveDirection = new Vector2(moveX,moveY);
     }
@@ -70,8 +90,59 @@ public class CharacterController : MonoBehaviour
 
     public void ChangePlayerLocalScale(float xScale)
     {
+       
         var scale = _transform.localScale;
         scale.x = xScale;
         _transform.localScale = scale;
+        gameManager.startTrail = false;
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag.Equals("door")) gameManager.SetActionPanelVisibility(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag.Equals("door")) gameManager.SetActionPanelVisibility(false);
+    }
+    public void ReadForTrail()
+    {
+       
+        transform.position = _trailPosition.position;
+        _animator.SetBool("walk", false);
+        _animator.SetBool("back", false);
+        _animator.SetBool("forward",true);
+        StartCoroutine(wait(2f));
+    }
+    private IEnumerator wait(float time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        _animator.enabled = false;
+    }
+    public void ChangeDress(List<Sprite> sprites)
+    {
+        if (productType==ProductType.CAP)
+        {
+            cap.sprite = sprites[0];
+        }
+        else if (productType == ProductType.SHIRT)
+        {
+            shirt.sprite = sprites[0];
+        }
+        else if (productType == ProductType.PANT)
+        {
+            pant.sprite = sprites[0];
+        }
+        else if (productType == ProductType.SHOES)
+        {
+           left_shoe.sprite = sprites[0];
+        }
+    }
+}
+enum ProductType
+{
+    CAP,
+    SHIRT,
+    PANT,
+    SHOES
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private CharacterController _characterController;
+    [SerializeField]
+    private GameObject _actionPanel;
+    [SerializeField]
+    private TextMeshProUGUI _popupMessage;
+    [SerializeField]
+    private GameObject _popup;
+    public static GameManager Instance;
+    private float _raycastDsitance =Mathf.Infinity;
+    public bool startTrail;
+    private void Awake()
     {
-        
+        Instance = this;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (startTrail&& Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
+
+            if (hit.transform != null)
+            {
+                Debug.Log("collider:"+ hit.transform.name);
+            }
+        }
+    }
+    public void StartTrial()
+    {
+        startTrail = true;
+        ShowMessage("Please Drag And Drop Your Dress Which One You Want.");
+        _characterController.ReadForTrail();
+    }
+    public void ShowMessage(string message)
+    {
+        _popup.SetActive(true);
+        _popupMessage.text = message;
+    }
+    public void SetActionPanelVisibility(bool visibility)
+    {
+        _actionPanel.SetActive(visibility);
     }
 }
